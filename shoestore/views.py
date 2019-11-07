a75e2c7eb106dbb498886b72dc616366679bd3f9
MungaiKeren <wambukeren@gmail.com>
2019-11-07 22:41:38 +0300

    This commit creates a delete icon that removes items from cart

--- shoestore/views.py
+++ shoestore/views.py
@@ -75,7 +75,7 @@ def remove_from_cart(request, slug):
             )[0]
             order.items.remove(order_item)
             messages.info(request, "This item was removed from cart")
-            return redirect("product", slug=slug)
+            return redirect("order_summary")
         else:
             messages.info(request, "This item was not in your cart")
             return redirect("product", slug=slug)
@@ -100,8 +100,11 @@ def remove_single_item_from_cart(request, slug):
                 user=request.user,
                 ordered=False
             )[0]
-            order_item.quantity -= 1
-            order_item.save()
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)    
             messages.info(request, "This item quantity was reduced")
             return redirect("order_summary")
         else:
